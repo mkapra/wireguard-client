400d44bf2d6edd4b3860edc9c44d9f87cd3874e3
CC3301 <none@nothing.null>
2021-10-05 16:58:12 +0200

    return actual data sample from api through simulate script, make GUI run with webserver

--- simulate/server.py
+++ simulate/server.py
@@ -6,35 +6,13 @@ app = Flask(__name__)
 @app.route("/peers", methods=['GET'])
 def peers():
 
-    sample_peers = {
-        "peers": [
-            {
-                "name": "mkapra_iphone",
-                "description": "IPhone von mkapra",
-                "interface": "wg0"
-            },
-            {
-                "name": "mkapra_iphone",
-                "description": "IPhone von mkapra",
-                "interface": "wg0"
-            },
-            {
-                "name": "mkapra_iphone",
-                "description": "IPhone von mkapra",
-                "interface": "wg0"
-            },
-            {
-                "name": "mkapra_iphone",
-                "description": "IPhone von mkapra",
-                "interface": "wg0"
-            },
-            {
-                "name": "mkapra_iphone",
-                "description": "IPhone von mkapra",
-                "interface": "wg0"
-            }
-        ]
-    }
+    sample_peers = [
+        {"id":"0","name": "mkapra_iphone","description": "IPhone von mkapra","interface_id": "0"},
+        {"id":"1","name": "mkapra_iphone","description": "IPhone von mkapra","interface_id": "0"},
+        {"id":"2","name": "mkapra_iphone","description": "IPhone von mkapra","interface_id": "0"},
+        {"id":"3","name": "mkapra_iphone","description": "IPhone von mkapra","interface_id": "0"},
+        {"id":"4","name": "mkapra_iphone","description": "IPhone von mkapra","interface_id": "0"}
+    ]
 
     if request.method == 'GET':
         response = app.response_class(
@@ -43,3 +21,28 @@ def peers():
             mimetype='application/json'
         )
         return response
+
+@app.route("/interfaces", methods=['GET'])
+def interfaces():
+    sample_interfaces = [
+        {"id":0,"priv_key":"123","pub_key":"1234"},
+        {"id":1,"priv_key":"qKD4n1KxaTw+tSAWfLCMudOidfQDR37zGQlLxEWdlXI=","pub_key":"tRrFijO98NDl5mKZCMx5Wj64ytY7u3nDkQFQwiYdQQY="},
+        {"id":2,"priv_key":"AJ/pcJt2TLU/UDplYVenobJKrAqrTzX33r4uXEy+v3U=","pub_key":"Cuq/iEgfGU0ZdTowlT9avd68+sX5BltBW+m2oBXPMws="},
+        {"id":3,"priv_key":"kGbMOyVwfbmTqIZPbyHvME9tvFtHL2/GWCTWteiKg1Y=","pub_key":"QYhDmmcOr75Ja+JxTzW37+hGGPYiJfiPJWrLglXgdwk="},
+        {"id":4,"priv_key":"uJiNRztTy/XHoXI5ZCE90Gh69kA5qlKVf61jcoc+L04=","pub_key":"VtPu0TcZ8blEbk5+GV1xCrjHLaZmCZuVghURcfeMpx4="},
+        {"id":5,"priv_key":"MOZhHyKmleW2gvFPFlYG+NcqbTP9UPr5nSJ/E3flU1A=","pub_key":"zkWD1g0Ley8vuvvPywQ9tfbtp+xuJ2zSmA/n7/GWASA="}
+   ]
+
+    if request.method == 'GET':
+        response = app.response_class(
+            response = json.dumps(sample_interfaces),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+
+
+
+
+
+
--- static/js/global.js
+++ static/js/global.js
@@ -1,4 +1,12 @@
 function getGlobals() {
+    api_url = global_get_api_url();
+    api_token = global_get_api_token();
+
+    if (api_url == '' || api_token == '') {
+        console.log("API Url and/or Token are not defined!");
+        window.location.replace("/views/login/index.html");
+    }
+
     document.getElementById('page_header_api_url').innerHTML = global_get_api_url();
 }
 
