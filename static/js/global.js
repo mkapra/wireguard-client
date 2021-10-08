f3738279470bdcecd328c75c8150fb0b79982a7a
Florian Fink <florianfink85@gmail.com>
2021-10-08 23:55:31 +0200

    new experimental table filling function, dns-server add and list works

--- static/js/global.js
+++ static/js/global.js
@@ -18,6 +18,7 @@ function global_get_api_token() {
     return localStorage.getItem('api_token');
 }
 
+// get with fetch function, returns the API endpoint's response to a GET reqeust
 async function get_with_fetch(endpoint) {
     api_token = global_get_api_token();
     api_url = global_get_api_url();
@@ -35,6 +36,7 @@ async function get_with_fetch(endpoint) {
     return result;
 }
 
+// post with fetch, will POST arbitrary data to the specified endpoint
 async function post_with_fetch(endpoint, data) {
     api_token = global_get_api_token();
     api_url = global_get_api_url();
@@ -53,4 +55,34 @@ async function post_with_fetch(endpoint, data) {
     const result = response.json();
 
     return result;
+}
+
+
+// Fill Any Table with data
+async function fillTable(endpoint, table_id) {
+
+    // get data from API
+    const response = await get_with_fetch(endpoint);
+
+    // get table and reset contents
+    var table = document.getElementById(table_id);
+    table.innerHTML = "";
+
+    // populate table
+    for (var item in response) {
+
+        // get the amount of cols to create
+        keys = Object.keys(response[item]);
+        len = keys.length;
+
+        // create row and cells
+        var row = table.insertRow(0);
+        var cols = []
+        for (var i = 0; i < len; i++) {
+            cols[i] = row.insertCell(i);
+            console.log(response[item][keys[i]]);
+            cols[i].innerHTML = response[item][keys[i]];
+            cols[i].style.border = "none";
+        }
+    }
 }
